819fcff43e3085837a8f99c3416d43b2b6821612
99003747 <78856378+99003747@users.noreply.github.com>
2021-02-25 17:44:24 +0530

    executed with multiple keywords

--- oops_python.py
+++ oops_python.py
@@ -1,16 +1,17 @@
 '''
 This file is to demonstrate the presence of user defined keywords
-
 Author:Ravikumar M Pise
 Contact:ravikumar.pise@ltts.com
 Date of creation:25/02/21
 '''
 
-import re  # To import regular expression
 
-class mini_project():  # to create Class
+import re  # To import regular expression
+n=input("enter no of inputs:")
+class Mini_project():  # to create Class
     def __init__(self):  # constructor
         self.count = 0  #initialize count as 0
+        #self.n=input("enter no of inputs:")
         self.in_file = open("input.txt", "r")  # to open input file
         self.keyword = input("enter input keyword:")  # to input keyword from user
         self.out_file_name = self.keyword + '.txt'  # to crete text file of name keyword
@@ -26,13 +27,14 @@ class mini_project():  # to create Class
                 string = file_list[i - 1] + " " + file_list[i] + " " + file_list[i + 1]  # to store previous and next word of keyword
                 self.out_file.write(string)  # to write string into out_file
                 self.out_file.write("\n")  # to separate line by line
-        self.out_file.write("Total number of " + self.keyword + " in input file are ")
+        self.out_file.write("Total number of " + self.keyword + " in input file are: ")
         self.out_file.write(str(self.count))  # to store number of keywords present in input file
     def close_file(self):  # function to close opened files
         self.in_file.close()  # to close in_file
         self.out_file.close()  # to close out_file
 
 
-obj = mini_project()  # to call class through object
-obj.read_function()   # to call read function
-obj.close_file()  # to call close function
+for i in range (int(n)):
+ obj = Mini_project()  # to call class through object
+ obj.read_function()   # to call read function
+ obj.close_file()  # to call close function
