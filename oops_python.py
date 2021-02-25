2e2bf8db12b72c631e305934e74f25464b258c53
99003747 <78856378+99003747@users.noreply.github.com>
2021-02-25 17:15:59 +0530

    inserted comments line by line

--- oops_python.py
+++ oops_python.py
@@ -1,31 +1,38 @@
-import re
+'''
+This file is to demonstrate the presence of user defined keywords
 
+Author:Ravikumar M Pise
+Contact:ravikumar.pise@ltts.com
+Date of creation:25/02/21
+'''
 
-class mini_project():
-    def __init__(self):
-        self.count = 0
-        self.in_file = open("input.txt", "r")
-        self.keyword = input("enter input keyword:")
-        self.out_file_name = self.keyword + '.txt'
-        self.out_file = open(self.out_file_name, 'w')
+import re  # To import regular expression
 
-    def read_function(self):
+class mini_project():  # to create Class
+    def __init__(self):  # constructor
+        self.count = 0  #initialize count as 0
+        self.in_file = open("input.txt", "r")  # to open input file
+        self.keyword = input("enter input keyword:")  # to input keyword from user
+        self.out_file_name = self.keyword + '.txt'  # to crete text file of name keyword
+        self.out_file = open(self.out_file_name, 'w')  # to open above created file
+
+    def read_function(self):  # to define function to read
         file_read = self.in_file.read()
-        file_list = re.split(r"\W+", str(file_read))
+        file_list = re.split(r"\W+", str(file_read))  # to split words of input file and to store it in a list
         for i in range(len(file_list)):
-            match = re.fullmatch(self.keyword, file_list[i], re.M | re.I)
+            match = re.fullmatch(self.keyword, file_list[i], re.M | re.I)  # to check match keyword to words of list
             if match:
-                self.count += 1
-                string = file_list[i - 1] + " " + file_list[i] + " " + file_list[i + 1]
-                self.out_file.write(string)
-                self.out_file.write("\n")
+                self.count += 1  # to increment count if keyword matches with word in list
+                string = file_list[i - 1] + " " + file_list[i] + " " + file_list[i + 1]  # to store previous and next word of keyword
+                self.out_file.write(string)  # to write string into out_file
+                self.out_file.write("\n")  # to separate line by line
         self.out_file.write("Total number of " + self.keyword + " in input file are ")
-        self.out_file.write(str(self.count))
-    def close_file(self):
-        self.in_file.close()
-        self.out_file.close()
+        self.out_file.write(str(self.count))  # to store number of keywords present in input file
+    def close_file(self):  # function to close opened files
+        self.in_file.close()  # to close in_file
+        self.out_file.close()  # to close out_file
 
 
-obj = mini_project()
-obj.read_function()
-obj.close_file()
+obj = mini_project()  # to call class through object
+obj.read_function()   # to call read function
+obj.close_file()  # to call close function
